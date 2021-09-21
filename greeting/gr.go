18029ae6ea211338b609d4f5270a518f6c4fe252
ilze weppenaar <aweooens@gmail.com>
2021-09-21 09:47:17 +0100

    made changes to greetings

--- greeting/gr.go
+++ greeting/gr.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+	fmt.Println("When running this program input 5 names after the command as arguments")
 	fmt.Println("There are:", len(os.Args)-1, "names")
 	fmt.Println("Hi", os.Args[1])
 	fmt.Println("Hi", os.Args[2])
