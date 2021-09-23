cd8d848b1cc917b85f0c2b3e17475f4080c9fb3a
ilze weppenaar <aweooens@gmail.com>
2021-09-23 19:04:14 +0100

    added evenOdd function

--- errex/movie.go
+++ errex/movie.go
@@ -7,6 +7,11 @@ import (
 )
 
 func main() {
+	oddEven()
+}
+
+func movie() {
+
 	if a := os.Args; len(a) != 2 {
 		fmt.Printf("Requires age\n")
 	} else if n, err := strconv.Atoi(a[1]); err != nil || n < 0 {
@@ -18,5 +23,20 @@ func main() {
 		fmt.Printf("PG-13\n")
 	} else {
 		fmt.Printf("PG-Rate\n")
+	}
+}
+
+func oddEven() {
+
+	if arg := os.Args; len(arg) != 2 {
+		fmt.Printf("Pick a number\n")
+	} else if n, err := strconv.Atoi(arg[1]); err != nil {
+		fmt.Printf("%q is not a number\n", arg[1])
+	} else if n%8 == 0 {
+		fmt.Printf("%d is an even number and devisible by 8\n", n)
+	} else if n%2 == 0 {
+		fmt.Printf("%d is an even number\n", n)
+	} else {
+		fmt.Printf("%d is an odd number\n", n)
 	}
 }
