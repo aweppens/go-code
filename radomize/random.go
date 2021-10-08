b59c883dfd0d79eb2f1a42e76fb9167b6a1c9bb2
ilze weppenaar <aweooens@gmail.com>
2021-10-08 07:43:07 +0100

    Added second game

--- radomize/random.go
+++ radomize/random.go
@@ -3,11 +3,16 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
+	"strconv"
 	"time"
 )
 
+const maxTurns = 10
+
 func main() {
-	luckyNumber()
+	//	luckyNumber()
+	luckyGuess()
 }
 
 func randomize() {
@@ -25,12 +30,44 @@ func luckyNumber() {
 	rand.Seed(time.Now().UnixNano())
 	guess, i := 10, 0
 
-	for n := 0; n != guess; n++ {
-		i += n
+	for n := 0; n != guess; {
+		i++
 		n = rand.Intn(guess + 1)
 		fmt.Printf("%d ", n)
 	}
 
-	fmt.Printf("It took %d amount of guessesl\n", i)
 	fmt.Println()
+	fmt.Printf("It took %d guesses\n", i)
+	fmt.Println()
+}
+
+func luckyGuess() {
+
+	rand.Seed(time.Now().UnixNano())
+	args := os.Args[1:]
+	if len(args) != 1 {
+		fmt.Println("Pick a number.")
+		return
+	}
+
+	guess, err := strconv.Atoi(args[0])
+	if err != nil {
+		fmt.Println("Not a number.")
+		return
+	}
+
+	if guess < 0 {
+		fmt.Println("Be positibe!")
+		return
+	}
+
+	for turn := 0; turn < maxTurns; turn++ {
+		n := rand.Intn(guess + 1)
+
+		if n == guess {
+			fmt.Println("YOU WIN !!!")
+			return
+		}
+	}
+	fmt.Println("YOU LSOT... Try Again?")
 }
